e8146447120883822421044b653f2e15d75c4097
TaeHoon02 <88dlaxogns@gachon.ac.kr>
2026-05-04 20:03:42 +0900

    add_list box

--- AI ChatBot/src/App.jsx
+++ AI ChatBot/src/App.jsx
@@ -121,9 +121,21 @@ function App() {
           >
             <option value="">🌍 지역 (전체)</option>
             <option value="서울특별시">서울특별시</option>
-            <option value="경기도">경기도</option>
+            <option value="부산광역시">부산광역시</option>
+            <option value="대구광역시">대구광역시</option>
             <option value="인천광역시">인천광역시</option>
+            <option value="광주광역시">광주광역시</option>
+            <option value="대전광역시">대전광역시</option>
             <option value="울산광역시">울산광역시</option>
+            <option value="경기도">경기도</option>
+            <option value="강원특별자치도">강원특별자치도</option>
+            <option value="충청북도">충청북도</option>
+            <option value="충청남도">충청남도</option>
+            <option value="전라북도특별자치도">전라북도특별자치도</option>
+            <option value="전라남도">전라남도</option>
+            <option value="경상북도">경상북도</option>
+            <option value="경상남도">경상남도</option>
+            <option value="제주특별자치도">제주특별자치도</option>
             <option value="재택근무">재택근무</option>
           </select>
 
